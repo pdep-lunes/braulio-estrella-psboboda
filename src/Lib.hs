3694e2bde0c187fb71416e3217462349706a8dca
Priscila <psboboda@frba.utn.edu.ar>
2025-04-28 12:20:25 -0300

    Agregue los personajes

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,3 +4,23 @@ import Text.Show.Functions ()
 
 doble :: Int -> Int
 doble x = x * 2
+
+restarMilDeVida :: Personaje -> Int
+restarMilDeVida unPersonaje = cantidadDeVida unPersonaje - 1000
+
+data Personaje = UnPersonaje {
+   nombre :: String,
+   poderBasico :: String,
+   superPoder :: String,
+   tieneSuperPoderActivo :: Bool,
+   cantidadDeVida :: Int,
+}
+
+espina :: Personaje 
+espina = UnPersonaje "Espina" "bola de espinas" "granada de espinas" True 4800 
+
+pamela :: Personaje
+pamela = UnPersonaje "Pamela" "lluvia de tuercas" "torreta curativa" False 9600 
+
+bolaEspinosa :: Personaje -> Personaje
+bolaEspinosa unPersonaje = UnPersonaje {cantidadDeVida = restarMilDeVida(unPersonaje)}
